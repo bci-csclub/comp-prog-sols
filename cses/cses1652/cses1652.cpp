9b8b413ac88fc18c41ccff62bd0789ce459323ad
Balint Rozsa <76824890+Balint-R@users.noreply.github.com>
2023-12-05 12:48:07 -0500

    Remove template

--- cses/cses1652/cses1652.cpp
+++ cses/cses1652/cses1652.cpp
@@ -1,35 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef unsigned uint;
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<int> vi;
-typedef vector<pii> vpii;
-typedef complex<double> cpx;
-template <typename T> using minPq = priority_queue<T, vector<T>, greater<T>>;
-#define ms(a, x) memset(a, x, sizeof(a))
-#define pb push_back
-#define fs first
-#define sn second
-#define ALL(v) begin(v), end(v)
-#define SZ(v) ((int) (v).size())
-#define lbv(v, x) (lower_bound(ALL(v), x) - (v).begin())
-#define ubv(v, x) (upper_bound(ALL(v), x) - (v).begin())
-template <typename T> inline void UNIQUE(vector<T> &v){sort(ALL(v)); v.resize(unique(ALL(v)) - v.begin());}
-const int INF = 0x3f3f3f3f;
-const ll LLINF = 0x3f3f3f3f3f3f3f3f;
-const double PI = acos(-1);
-#define FR(i, n) for(int i = 0; i < (n); i++)
-#define FOR(i, a, b) for(int i = (a); i < (b); i++)
-#define FORR(i, a, b) for(int i = (a); i >= (b); i--)
-#define dbg(x) {cerr << #x << ' ' << x << endl;}
-#define dbgArr(arr, n) {cerr << #arr; FR(_i, n) cerr << ' ' << (arr)[_i]; cerr << endl;}
-template <typename T, typename U>
-ostream& operator<<(ostream &os, pair<T, U> p){return os << "(" << p.fs << ", " << p.sn << ")";}
-
 const int MN = 1005;
 int n, q;
 int pref[MN][MN];
